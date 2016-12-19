0d42bdf96191b1264870ad8ea35b67a1a820f73e
namadaza <aman.s.azad@gmail.com>
2016-12-19 00:21:31 -0800

    CSS is STRONG lol, abstracted collection styling, using image keeps square ratio

--- src/components/HipHopCollection.js
+++ src/components/HipHopCollection.js
@@ -5,18 +5,27 @@ export default class HipHopCollection extends React.Component {
 
   render() {
     return (
-      <section className="bg-primary" id="hiphopcollection">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-lg-offset-2 text-center">
-              <h2 className="section-heading">Hip Hop Collection</h2>
-              <hr className="light"></hr>
-              <div id="row">
-                <div className="tile"></div>
-                <div className="tile"></div>
-                <div className="tile">Hover Me</div>
-                <div className="tile"></div>
-                <div className="tile"></div>
+      <section className="bg-primary collection" id="collection">
+        <div className="row">
+          <div className="col-lg-8 text-center">
+            <h2 className="section-heading">Hip Hop Collection</h2>
+            <hr className="light"></hr>
+            <div id="row">
+              <div className="tile">
+                <img src="img/JEFFREY_album.jpg" alt="JEFFREY"></img>
+              </div>
+              <div className="tile">
+                <img src="img/JEFFREY_album.jpg" alt="JEFFREY"></img>
+              </div>
+              <div className="tile">
+                Hover Over Me!
+                <img src="img/JEFFREY_album.jpg" alt="JEFFREY"></img>
+              </div>
+              <div className="tile">
+                <img src="img/JEFFREY_album.jpg" alt="JEFFREY"></img>
+              </div>
+              <div className="tile">
+                <img src="img/JEFFREY_album.jpg" alt="JEFFREY"></img>
               </div>
             </div>
           </div>
--- src/components/RBCollection.js
+++ src/components/RBCollection.js
@@ -5,17 +5,21 @@ export default class RBCollection extends React.Component {
 
   render() {
     return (
-      <section className="bg-primary" id="rbcollection">
-        <div className="container">
-          <div className="row">
-            <div className="col-lg-8 col-lg-offset-2 text-center">
-              <h2 className="section-heading">R&B Collection</h2>
-              <hr className="light"></hr>
-                <p className="text-faded">Start Bootstrap has everything you need to get your new website up and running in no time! All of the templates and themes on Start Bootstrap are open source, free to download, and easy to use. No strings attached!</p>
-              </div>
+      <section className="bg-primary collection" id="collection">
+        <div className="row">
+          <div className="col-lg-8 text-center">
+            <h2 className="section-heading">R&B Collection</h2>
+            <hr className="light"></hr>
+            <div id="row">
+              <div className="tile"></div>
+              <div className="tile"></div>
+              <div className="tile">Hover Me</div>
+              <div className="tile"></div>
+              <div className="tile"></div>
             </div>
           </div>
-        </section>
+        </div>
+      </section>
     );
   }
 }
